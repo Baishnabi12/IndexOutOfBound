ce50fda9b6d9ddc23784f1e626bb7798760b1fb1
baish <baish@BAISHNABI>
2024-10-17 13:31:02 +0530

    Add project files.

---
+++ IndexOutOfBound/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program {
+    static void Main(String[] args)
+    {
+        int[] num = { 1, 2, 3, 4 };
+        try
+        {
+            for (int i = 0; i > num.Length; i++)
+            {
+                Console.WriteLine(num[5]);
+            }
+        }
+        catch (IndexOutOfRangeException ex)
+        {
+            Console.WriteLine("Index Out Of Range");
+        }
+       
+    }
+}
+
